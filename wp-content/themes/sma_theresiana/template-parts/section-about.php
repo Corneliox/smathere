4ceef12ec66f352ddc59dc9a6076227f2c284266
Cornelio <cornelioabdimash@gmail.com>
2026-06-25 17:18:58 +0800

    Fix YouTube video height by removing 4:5 aspect ratio class

--- wp-content/themes/sma_theresiana/template-parts/section-about.php
+++ wp-content/themes/sma_theresiana/template-parts/section-about.php
@@ -139,7 +139,7 @@ $visimisi = [
 					decoding="async"
 				>
 				<?php else : ?>
-				<figure class="wp-block-embed-youtube wp-block-embed is-type-video is-provider-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio th-about__image" style="margin: 0; overflow: hidden; border-radius: var(--th-radius-xl); box-shadow: var(--th-shadow-xl);">
+				<figure class="wp-block-embed-youtube wp-block-embed is-type-video is-provider-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio" style="margin: 0; width: 100%; overflow: hidden; border-radius: var(--th-radius-xl); box-shadow: var(--th-shadow-xl);">
 					<div class="wp-block-embed__wrapper" style="position: relative; padding-bottom: 56.25%; height: 0;">
 						<iframe title="Profil SMA Theresiana 1 Semarang 2026" width="960" height="540" src="https://www.youtube.com/embed/rEfp5Cywj9k?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen="" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
 					</div>
